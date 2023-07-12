5e12a5de0434486bdff0521e7ab6ac5f0e97659c
sabeet <sabeet.other.way@gmail.com>
2023-07-12 03:00:49 -0400

    first commit

---
+++ creative-portfolio/src/App.jsx
@@ -0,0 +1,88 @@
+import './App.css'
+import TextSwap from './TextSwap'
+
+function App() {
+
+  return (
+    <>
+        <div className='wrapper'>
+            <div className="section">
+                <div className="columns">
+
+                    <div className="col col-100">
+                        <span className="subtext">I am the product of my environment</span>
+                    </div>
+
+                    <div className="col col-50">
+                        <span>So let me show you,</span>
+                    </div>
+
+                    <div className="col col-50">
+                        <span>what that means</span>
+                    </div>
+
+
+                    <div className="col col-33">
+                        <hr />
+                    </div>
+
+                    <div className="col col-33">
+                        <span>Experience</span>
+                    </div>
+
+
+                    <div className="col col-33">
+                        <hr />
+                    </div>
+
+                    <div className="col col-33">
+                        Capital One
+                    </div>
+
+                    <div className="col col-33">
+                        Ally Financial
+                    </div>
+
+                    <div className="col col-33">
+                        Workschool.co
+                    </div>
+
+                    <div className="col col-33">
+                        <hr />
+                    </div>
+
+                    <div className="col col-33">
+                        <span>Projects</span>
+                    </div>
+
+                    <div className="col col-33">
+                        <hr />
+                    </div>
+
+                    <div className="col col-25">
+                        Discord Bot                   
+                    </div>
+
+                    <div className="col col-25">
+                        QC Prof Stat
+                    </div>
+
+                    <div className="col col-25">
+                        Unity Ball Runner
+                    </div>
+
+                    <div className="col col-25">
+                        Hit Me
+                    </div>
+                </div>
+            </div>
+
+            
+        </div>
+
+        <script src="./TextSwap.js"></script>
+    </>
+  )
+}
+
+export default App
---
+++ creative-portfolio/src/TextSwap.js
@@ -0,0 +1,23 @@
+import { gsap } from "gsap";
+
+function TextSwap(){
+    // create a reusable effect that swaps text
+gsap.registerEffect({
+    name: "swapText",
+    effect: (targets, config) => {
+      let tl = gsap.timeline({delay: config.delay});
+      tl.to(targets, {opacity: 0, duration: config.duration / 2});
+      tl.add(() => targets[0].innerText = config.text);
+      tl.to(targets, {opacity: 1, duration: config.duration});
+      return tl;
+    },
+    defaults: {duration: 1}, 
+    extendTimeline: true
+});
+
+
+var tl = gsap.timeline({repeat: 0});
+tl.swapText(".subtext", {text: "THE SECOND INFO TEXT 1 IS THIS ONE", delay: 2})
+}
+
+export default TextSwap
